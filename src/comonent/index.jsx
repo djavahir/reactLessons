9befd8a4fa9cbaee38c661af4fbbecc6276377d1
Javohir Turgunpolatov <javohirturgunpolatov505@gmail.com>
2022-11-02 14:55:16 +0500

    ClassG6 add info

--- src/comonent/index.jsx
+++ src/comonent/index.jsx
@@ -5,7 +5,9 @@ import React,{ Component } from "react";
  class ClassG6 extends Component{
     render(){
         return(
-<div></div>
+<div>
+    <h1>Salom Javohitr </h1>
+</div>
         )
     }
 
