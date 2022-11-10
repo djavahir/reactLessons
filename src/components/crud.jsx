c5e0fba4a55779b2ca852e2235faea8468c008ac
Javohir Turgunpolatov <javohirturgunpolatov505@gmail.com>
2022-11-10 12:47:49 +0500

    malumotlarni map qilish

---
+++ src/components/crud.jsx
@@ -0,0 +1,9 @@
+export const students = [
+    { id: 1, name: 'Lochinbek', job: 'Student' },
+    { id: 2, name: 'Kamolddin', job: 'Student' },
+    { id: 3, name: 'Sobirjon', job: 'Student' },
+    { id: 4, name: 'Javohir', job: 'Student' },
+    { id: 5, name: 'Abdulaziz', job: 'Student' },
+    { id: 6, name: 'Begzod', job: 'Student' },
+    { id: 7, name: 'Bexruz', job: 'Student' },
+]
--- src/get.jsx
+++ src/get.jsx
@@ -1,28 +1,51 @@
 import React from "react";
-
+import { students } from "./components/crud";
 class GetName extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: "",
       surname: "",
+      data: students,
     };
   }
   render() {
-    const onChange=(event)=>{
-      this.setState({[event.target.name]:event.target.value})
-    }
+    const onChange = (event) => {
+      this.setState({ [event.target.name]: event.target.value });
+    };
+    const onSearch = (event) => {
+      let search = students.filter((value) =>value.name.includes(event.target.value));
+      this.setState({
+        data:search,
+      })
+    };
 
     return (
       <div>
         <h1>Name:{this.state.name}</h1>
         <h1>Surname:{this.state.surname}</h1>
         <hr />
-        <br />
         <input onChange={onChange} type="text" name="name" placeholder="name" />
-        <br /><br />
-        <br /><br />
-        <input onChange={onChange} type="text" name="surname" placeholder="surname"/>
+        <br />
+        <br />
+        <input
+          onChange={onChange}
+          type="text"
+          name="surname"
+          placeholder="surname"
+        />
+        <hr />
+        <br />
+        <input onChange={onSearch} type="text" placeholder="search" />
+        {this.state.data.map(({ id, name, job }) => {
+          return (
+            <h1 key={id}>
+              {id}
+              {name}
+              {job}
+            </h1>
+          );
+        })}
       </div>
     );
   }
